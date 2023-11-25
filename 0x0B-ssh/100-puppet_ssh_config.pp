8bcbf0bbe4a51e71ff6858aefe811d1092a32e6b
Mcrymbo <croco2023@gmail.com>
2023-11-25 20:57:16 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,6 @@
 
 exec { 'echo':
   path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_conf',
+  command => 'echo "    IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> sudo etc/ssh/ssh_conf',
   returns => [0,1],
 }
