0e27cb23c0399073a2e54a8ca6ac4a9d1eb76cd2
Mcrymbo <croco2023@gmail.com>
2023-11-28 12:50:23 +0300

    puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# installing and configuring nginx
+
+include stdlib
+
+package { 'nginx':
+  ensure => installed,
+}
+
+#adding redirect
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/Mcrymbo permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => [Package['nginx'], File_line['redirect']],
+}
