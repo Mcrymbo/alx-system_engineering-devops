4af2bb0289c141b679186d6e383db2a8962fc564
Mcrymbo <croco2023@gmail.com>
2024-01-02 13:21:47 +0300

    correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,23 @@ import requests
 import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     base_url = 'https://jsonplaceholder.typicode.com/'
 
-    user_id = sys.argv[1]
-    user = '{}users/{}'.format(base_url, user_id)
-    user_res = requests.get(user)
-    user_obj = user_res.json()
-    print('Employee {} is done with tasks'
+    user = '{}users/{}'.format(base_url, sys.argv[1])
+    res = requests.get(user)
+    user_obj = res.json()
+    print("Employee {} is done with tasks"
           .format(user_obj.get('name')), end="")
 
-    todos = '{}todos?userId={}'.format(base_url, user_id)
+    todos = '{}todos?userId={}'.format(base_url, sys.argv[1])
     todo_res = requests.get(todos)
     todo_obj = todo_res.json()
-
     tasks = []
     for task in todo_obj:
         if task.get('completed') is True:
             tasks.append(task)
 
-    print('({}/{}):'.format(len(tasks), len(todo_obj)))
-    for task in todo_obj:
-        print('\t {}'.format(task.get('title')))
+    print("({}/{}):".format(len(tasks), len(todo_obj)))
+    for task in tasks:
+        print("\t {}".format(task.get("title")))
